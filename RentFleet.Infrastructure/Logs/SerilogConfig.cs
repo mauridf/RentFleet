5507ae25bb6cc92981d2c1115301e4d7d7f226ec
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2025-03-01 12:44:56 -0300

    Configurando para além de jogar na Console e no ElasticSearch também criar um arquivo de Log

---
+++ RentFleet.Infrastructure/Logs/SerilogConfig.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace RentFleet.Infrastructure.Logs
+{
+    public static class SerilogConfig
+    {
+        public static void Configure()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = "rentfleet-logs-{0:yyyy.MM.dd}"
+                })
+                .WriteTo.File(
+                    path: "logs/rentfleet-log-.txt", // Caminho do arquivo de log
+                    rollingInterval: RollingInterval.Day, // Cria um arquivo por dia
+                    retainedFileCountLimit: 7, // Mantém os últimos 7 arquivos
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                )
+                .CreateLogger();
+        }
+    }
+}
